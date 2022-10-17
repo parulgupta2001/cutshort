4e558e42dd62a2f9ead16582181749ac8b91d99f
Parul Gupta <94689580+parulgupta2001@users.noreply.github.com>
2022-10-17 22:57:57 +0530

    feat - progress indicator component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import {
+  ProgressIndicator,
+  StepOne,
+  StepTwo,
+  StepThree,
+  StepFour,
+} from "./components/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<ProgressIndicator />}>
+        {/* <Route path="/home/step1" element={<StepOne />} />
+        <Route path="/home/step2" element={<StepTwo />} />
+        <Route path="/home/step3" element={<StepThree />} />
+        <Route path="/home/step4" element={<StepFour />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
